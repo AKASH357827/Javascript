65ecba2d79cc115d75eb75c09cf64b344d1ef24e
akash ln <akashln357@gmail.com>
2025-01-30 16:38:49 +0530

    dfbg

---
+++ script3.js
@@ -0,0 +1,9 @@
+let a=prompt("enter the first number")
+let b=prompt("enter the second number")
+if(a[a.length-1]==b[b.length-1])
+{
+    console.log("yes they have same dights")
+}
+else{
+    console.log("they dont have same")
+}
