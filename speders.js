65c0f99ff4ea7c146aa49ffa7303a31f809cc7e4
akash ln <akashln357@gmail.com>
2025-09-01 17:02:06 +0530

    hi

---
+++ speders.js
@@ -0,0 +1,13 @@
+// let a=[1,2,3,4,5];
+// console.log(a.pop());
+// console.log(a.push(7));
+// console.log(a);
+// console.log(a.indexOf());
+// let a=[2,4542,24,24,55];
+// let c=a.sort((a,b)=>{
+//     return a-b;
+// })
+// 
+// console.log(a);console.log(c);
+let [a,b]=[10,20,["this",["is"],["nested"]],"array"];
+console.log(a); 
